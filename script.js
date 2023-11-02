70fa30e52a0e6a2c5d76a10c107acc66f70e6bf6
Divyansh <divyanshrajpoot112@gmail.com>
2023-11-02 15:19:21 +0530

    DOM Project 1

---
+++ script.js
@@ -0,0 +1,12 @@
+const containerVar = document.getElementById("container");
+console.log(containerVar);
+const style = `
+    width: 300px;
+    height: 200px;
+    margin: 20px;
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+    background-color: yellow;
+`
+container.setAttribute("style", style);
